f8adb0fb205b6e6fd54ce7a811e32b6016da50bc
salma154 <salmaabdooo154@gmail.com>
2023-12-04 13:22:18 +0200

    added

--- salma.java
+++ salma.java
@@ -1,5 +1,5 @@
-pubilc class salma{
-    pubilc static void main (String[] args){
+public class Salma {
+    public static void main (String[] args) {
         System.out.println("salma");
     }
 }
